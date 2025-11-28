5d8d815b2aacd14ce6afea9a95c43c7d5bd8d7b4
JB <j-baptiste.largeron@etu.uca.fr>
2025-11-28 15:24:05 +0100

    Test des trois fonctions

--- TP3/Cannibales/Exercise.java
+++ TP3/Cannibales/Exercise.java
@@ -1,5 +1,4 @@
 package Cannibales;
-import java.util.List;
 
 public class Exercise {
     public static void main(String[] args) {
@@ -8,15 +7,28 @@ public class Exercise {
         int NB_CANN = 3;
         
         CannibalProblem problem = new CannibalProblem(NB_MISS, NB_CANN);
-        List<Node> solutionNodes = ResearchAlgorithm.search(problem);
-        for (Node solutionNode : solutionNodes) {
+        Node solutionNode = ResearchAlgorithm.search(problem);
             if (solutionNode != null) {
                 System.out.println("Search has found a solution:");
                 solutionNode.printSolution();
             } else {
                 System.out.println("Could not find a solution.");
             }
-        }
-        System.out.println(String.format(" NB solutions totales %d", solutionNodes.size()) );
+        
+        solutionNode = ResearchAlgorithmDFS.search(problem);
+            if (solutionNode != null) {
+                System.out.println("DDFS Search has found a solution:");
+                solutionNode.printSolution();
+            } else {
+                System.out.println("Could not find a solution.");
+            }
+        solutionNode = ResearchAlgorithm.search(problem);
+            if (solutionNode != null) {
+                System.out.println("DFS Iter Search has found a solution:");
+                solutionNode.printSolution();
+            } else {
+                System.out.println("Could not find a solution.");
+            }
+        
     }
 }
