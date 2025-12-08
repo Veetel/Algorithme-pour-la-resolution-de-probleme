ab3daa0b322820c4dda3259231f8f27b88d556a8
JB <j-baptiste.largeron@etu.uca.fr>
2025-12-08 11:29:48 +0100

    bug fix

--- TP6/Game.java
+++ TP6/Game.java
@@ -1,12 +1,18 @@
 package TP6;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.Scanner;
 
 public class Game {
     int N;
-    Map<Integer,Function<State,Action>> policies;
+    Map<Integer,Function<State,State.Action>> policies;
 
     public Game(int N) {
         if (N >= 0) {
             this.N = N;
+            this.policies = new HashMap<>();
+            this.policies.put(1,minimaxPolicy);
         } else {
             throw new IllegalArgumentException("N ne peut pas etre negatif");
         }
@@ -20,7 +26,7 @@ public class Game {
         return s.isGoal();
     }
 
-    Action customPolicy(State s) {
+    State.Action customPolicy(State s) {
         int input = 0;
         Scanner thatScan = new Scanner(System.in);
         System.out.println("choisir 1 -> n-1 ou 2->n/2");
@@ -30,10 +36,10 @@ public class Game {
         return s.actions().get(input);
     }
 
-    Action minimaxPolicy(State s) {
+    State.Action minimaxPolicy(State s) {
         int utility = 0;
-        Action out;
-        for (Action a : s.actions()) {
+        State.Action out = null;
+        for (State.Action a : s.actions()) {
             int childUtility = s.succ(a).utility();
             if ((utility == 0) || ((childUtility < utility) ^ (s.player == 1))) {
                 utility = childUtility;
@@ -45,9 +51,9 @@ public class Game {
 
     int play() {
         State s = this.initialState();
-        while (!this.isOver()) {
+        while (!this.isOver(s)) {
             s.display();
-            s = s.succ(policies.get(s.player)(s));
+            s = s.succ(policies.get(s.player).apply(s));
         }
         System.out.println(String.format("Le joueur %d a gagné", s.player));
         return s.player;
